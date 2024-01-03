5f26d8a1c2815c0e559e062f3380a926e12eb15c
Rufa <rufaglva@gmail.com>
2024-01-03 19:22:58 +0400

    added css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import FlashcardList from "./FlashcardList";
+import './app.css'
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
@@ -13,17 +14,23 @@ const SAMPLE_FLASHCARDS = [
     id: 1,
     question: 'What is 2 + 2?',
     answer: '4',
+    options: [
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
   },
   {
     id: 2,
     question: 'Question 2',
     answer: 'Answer',
+    options: [
+      "Answer 1",
+      "Answer 2",
+      "Answer 3",
+    ]
   },
-  {
-    id: 1,
-    question: 'Question 3',
-    answer: 'Answer',
-  }
 ]
 
 export default App;
--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function Flashcard() {
+export default function Flashcard({ flashcard }) {
+  const [flip, setFlip] = useState(false);
+
   return (
-    <div>
-      
+    <div
+      className={`card ${flip ? 'flip' : ''}`}
+      onClick={() => setFlip(!flip)}
+    >
+      <div className="front">
+        {flashcard.question}
+        <div className="flashcard-options">
+          {flashcard.options ? (
+            flashcard.options.map((option, index) => (
+              <div key={index} className="flashcard-option">
+                {option}
+              </div>
+            ))
+          ) : (
+            <div>No options available</div>
+          )}
+        </div>
+      </div>
+      <div className="back">{flashcard.answer}</div>
     </div>
-  )
+  );
 }
