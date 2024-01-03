8c8673d3bf79886620d041118c0a7ed99a7fe3d9
Rufa <rufaglva@gmail.com>
2024-01-03 19:37:29 +0400

    connected to opentrivia API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import FlashcardList from "./FlashcardList";
 import './app.css'
+import axios from "axios";
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+
+  useEffect(() => {
+    axios
+    .get('https://opentdb.com/api.php?amount=10')
+    .then(res => {
+      setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = questionItem.correct_answer
+        const options = [...questionItem.incorrect_answers, answer]
+        return {
+          id: `${index}-${Date.now()}`,
+          question: questionItem.question,
+          answer: questionItem.correct_answer,
+          options: options.sort(() => Math.random()- .5)
+        }
+      }))
+      console.log(res.data)
+    })
+  }, [])
+
   return (
     <FlashcardList flashcards={flashcards} />
   );
